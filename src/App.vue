70cb70ffc56823e6f78e601877dbf90c9ca0f669
miffy <plus@outlook.at>
2021-12-20 18:58:57 +0800

    23

--- src/App.vue
+++ src/App.vue
@@ -3,118 +3,31 @@ export default {
   name: 'App',
   data() {
     return {
-      formValues: {
-        name: '',
-        profileSummary: '',
-        country:'',
-        jobLocation: [],
-        remoteWork: 'no',
-        skillSet: [],
-        yearsOfExperience: '',
-        age: null,
-      }
+      firstName: 'Sooia',
+      lastName: 'Miffy',
+      items: [
+        {id:1, title: 'TV', price: 100},
+        {id:2, title: 'Phone', price: 200},
+        {id:3, title: 'Laptop', price: 300}
+      ]
     };
   },
   methods: {
-    submitForm(){
-      console.log('Form values', this.formValues);
-      alert('已经提交以下信息'+JSON.stringify(this.formValues, null, 2))
+  },
+  computed: {
+    fullName(){
+      return `${this.firstName} ${this.lastName}`
     }
-
   }
 };
 </script>
 
 <template>
-  <div>
-    <pre>
-    {{ JSON.stringify(formValues, null, 2)}}
-    </pre>
-  </div>
-
-  <form @submit.prevent="submitForm">
-    <div>
-      <label for="name">Name</label>
-      <input type="text" id="name" v-model.lazy.trim="formValues.name">
-    </div>
-
-    <br>
-
-    <div>
-      <label for="profile">Profile Summary</label>
-      <textarea id="profile" cols="30" rows="10" v-model="formValues.profileSummary"></textarea>
-    </div>
-
-    <br>
-
-    <div>
-      <label for="country">Country</label>
-      <select id="country" v-model="formValues.country">
-        <option value="">Select a country</option>
-        <option value="india">India</option>
-        <option value="vietnam">Vietnam</option>
-        <option value="singapore">Singapore</option>
-      </select>
-    </div>
-
-    <br>
-
-    <div>
-      <label for="jon-location">job Location</label>
-      <select id="job-location" multiple v-model="formValues.jobLocation">
-        <option value="india">India</option>
-        <option value="vietnam">Vietnam</option>
-        <option value="singapore">Singapore</option>
-      </select>
-    </div>
-
-    <br>
-
-    <div>
-      <input type="checkbox" id="remoteWork" v-model="formValues.remoteWork" true-value="yes" false-value="no">
-      <label for="remoteWork">Open to Remote Work?</label>
-    </div>
-
-    <br>
-
-    <div>
-      <label>Skill Set</label>
-      <input type="checkbox" id="html" value="html" v-model="formValues.skillSet" />
-      <label for="html">HTML</label>
-      <input type="checkbox" id="css" value="css" v-model="formValues.skillSet"  />
-      <label for="css">CSS</label>
-      <input type="checkbox" id="javascript" value="javascript" v-model="formValues.skillSet" />
-      <label for="javascript">JavaScript</label>
-    </div>
-
-    <br>
-
-    <div>
-      <label>Years of Experience</label>
-
-        <input type="radio" id="0-2" value="0-2" v-model="formValues.yearsOfExperience"/>
-        <label for="0-2">0-2</label>
-
-        <input type="radio" id="3-5" value="3-5" v-model="formValues.yearsOfExperience"/>
-        <label for="3-5">3-5</label>
-        
-        <input type="radio" id="5-10" value="5-10" v-model="formValues.yearsOfExperience"/>
-        <label for="5-10">5-10</label>
-        
-        <input type="radio" id=">10" value=">10" v-model="formValues.yearsOfExperience"/>
-        <label for=">10">>10</label>
-        
-    </div>
-
-    <div>
-      <label for="age">Age</label>
-      <input type="number" id="age" v-model.number="formValues.age">
-    </div>
-
-    <div>
-      <button>Submit</button>
-    </div>
-  </form>
+  <h2>Fullname - {{firstName}} {{lastName}}</h2>
+  <h2>Fullname - {{fullName}}</h2>
+  <h2>
+    Total - {{items.reduce((total, curr) => (total = total + curr.price), 0)}}
+  </h2>
 </template>
 
 <style>
